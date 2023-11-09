f2770888eab2ba320470df95c56648d806022e28
pedroeperezn <pedroeperezn@gmail.com>
2023-11-08 22:25:12 -0800

    testing stuff

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contentContainer = document.querySelector(".gallery-content");
+    const contentItems = document.querySelectorAll(".content");
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+    let currentIndex = 0;
+  
+    nextButton.addEventListener("click", function () {
+      currentIndex = (currentIndex + 1) % contentItems.length;
+      updateGallery();
+    });
+  
+    prevButton.addEventListener("click", function () {
+      currentIndex = (currentIndex - 1 + contentItems.length) % contentItems.length;
+      updateGallery();
+    });
+  
+    function updateGallery() {
+      const translateX = -currentIndex * 100 + "%";
+      contentContainer.style.transform = `translateX(${translateX})`;
+    }
+  });
